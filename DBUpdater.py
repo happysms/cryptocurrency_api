63456c786e05e1a04a9ab3c8f275e5a8958b98fa
minsung <khuminsung@khu.ac.kr>
2021-10-04 15:36:31 +0900

    db update 기능 모두 구현, 코드리펙토링, 성능 개선 후  테스트 필요

--- DBUpdater.py
+++ DBUpdater.py
@@ -1,31 +1,118 @@
 import pymysql
-
+import json
+from datetime import datetime, timedelta, timezone
+import pandas as pd
+import pyupbit
+import time
+import logging
 
 class DBUpdater:
+
     def __init__(self):
         self.conn = pymysql.connect(host='cryptocurrencydatabase.c5h79dp2k6f7.ap-northeast-2.rds.amazonaws.com',
                                     user='admin',
                                     password='khuminsung12!',
-                                    db='cyripto',
+                                    db='crypto',
                                     charset='utf8')
 
-        with self.conn.cursor() as curs:
-            crypto_list = ['KRW_BTC', 'KRW_ETH', "KRW_XRP", "KRW_ADA", "KRW_DOT", "KRW_DOGE"]
+        self.logger = logging.getLogger()
+        self.logger.setLevel(logging.INFO)
+        self.cryptos = ['KRW-BTC', 'KRW-ETH', "KRW-XRP", "KRW-ADA", "KRW-DOT", "KRW-DOGE"]
+        self.crypto_tables = ['KRW_BTC', 'KRW_ETH', "KRW_XRP", "KRW_ADA", "KRW_DOT", "KRW_DOGE"]
 
-            for crypto in crypto_list:
+        with self.conn.cursor() as curs:
+            for crypto in self.crypto_tables:
                 sql = f"""
                 CREATE TABLE IF NOT EXISTS {crypto} (
-                    id INT PRIMARY KEY,
-                    Datetime TIMESTAMP,
-                    open BIGINT(20),
-                    high BIGINT(20),
-                    low BIGINT(20),
-                    close BIGINT(20),
-                    volume BIGINT(20))
+                    Datetime TIMESTAMP PRIMARY KEY,
+                    open DOUBLE,
+                    high DOUBLE,
+                    low DOUBLE,
+                    close DOUBLE,
+                    volume DOUBLE)
                 """
                 curs.execute(sql)
 
         self.conn.commit()
 
-    def __del__(self):
-        self.conn.close()
+    def execute_daily(self):
+        """utc 00시 00분에 가격데이터 업데이트"""
+
+        try:  # configure 파일이 있다면 이전 업데이트 날짜 데이터를 가져오고, 최신화된 날짜인 오늘 날짜로 수정한다.
+            with open('config.json', 'r') as in_file:
+                config = json.load(in_file)
+                dates_to_fetch = config['dates_to_fetch']
+
+            with open('config.json', 'w') as out_file:
+                config = {"dates_to_fetch": (datetime.now(timezone.utc)).strftime("%Y-%m-%d")}
+                json.dump(config, out_file)
+
+        except FileNotFoundError:  # 만약 없다면 dates_to_fetch 변수에 2년 전 날짜를 할당한 후, configure 파일을 오늘 날짜로 수정한다.
+            with open('config.json', 'w') as out_file:
+                dates_to_fetch = (datetime.now() - timedelta(3)).strftime("%Y-%m-%d")
+                config = {"dates_to_fetch": (datetime.now(timezone.utc)).strftime("%Y-%m-%d")}
+                json.dump(config, out_file)
+
+        self.update_daily_price(dates_to_fetch)
+
+    def update_daily_price(self, dates_to_fetch):
+
+        def generate_date_range(start, end):
+            start = datetime.strptime(start, "%Y-%m-%d")
+            end = datetime.strptime(end, "%Y-%m-%d")
+            for i in range(1, (end - start).days + 1):
+                yield (start + timedelta(days=i)).strftime("%Y-%m-%d")
+
+        for crypto in self.cryptos:
+            for date in generate_date_range(dates_to_fetch, datetime.now().strftime("%Y-%m-%d")):
+                daily_df, check = self.get_daily_crypto_data(crypto, date)
+
+                if check:  # 데이터가 없는 날도 존재하기 때문. ex) 상장되기 전
+                    self.replace_into_db(daily_df, crypto)
+
+    def replace_into_db(self, df, crypto):
+        crypto = crypto.replace("-", "_")
+
+        with self.conn.cursor() as curs:
+            for r in df.itertuples():
+                sql = "REPLACE INTO {} VALUES ('{}', {}, {}, {}, {}, {})".format(crypto, str(r.datetime),
+                                                                                 r.open, r.high, r.low,
+                                                                                 r.close, r.volume)
+                curs.execute(sql)
+            self.conn.commit()
+        print("[{}] REPLACE INTO {}".format(df.index[0].strftime('%Y-%m-%d'), crypto))
+
+    def get_missing_value_frame(self, df):
+        day = df.index[0].strftime("%Y-%m-%d")
+        next_day = self.get_next_day(day)
+        df = df[f'{day} 09:00:00':f'{next_day} 08:59:59']
+        df = df.resample(rule='min').last()
+        df = df.fillna(method='ffill')
+        return df
+
+    def get_next_day(self, date: str) -> str:
+        tomorrow = datetime.strptime(date, '%Y-%m-%d') + timedelta(1)
+        return tomorrow.strftime('%Y-%m-%d')
+
+    def get_daily_crypto_data(self, crypto, date):
+        check = False
+        try:
+            df = self.get_missing_value_frame(pyupbit.get_ohlcv(ticker=crypto, count=1440,
+                                              to=datetime.strptime(f"{date} 9:00:00", "%Y-%m-%d %H:%M:%S"),
+                                              interval="minute1"))
+            df['datetime'] = df.index
+            check = True
+            time.sleep(2)
+            return df, check
+
+        except Exception:
+            self.logger.error(f"{crypto}의 {date} 기간 데이터가 존재하지 않습니다.")
+            return None, check
+
+    # def __del__(self):
+    #     self.conn.close()
+
+if __name__ == "__main__":
+    db = DBUpdater()
+    db.execute_daily()
+
